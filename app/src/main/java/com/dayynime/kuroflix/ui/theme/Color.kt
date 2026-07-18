0da31fc48fab7a23cd12374e29497d089d342888
RMBLOGG <ramdanhidayah176@gmail.com>
2026-07-19 06:14:37 +0700

    add preview

--- app/src/main/java/com/dayynime/kuroflix/ui/theme/Color.kt
+++ app/src/main/java/com/dayynime/kuroflix/ui/theme/Color.kt
@@ -7,17 +7,17 @@ val DarkBg = Color(0xFF0A0A0B)
 val DarkSurface = Color(0xFF141416)
 val DarkSurfaceVariant = Color(0xFF1D1D20)
 
-// Satu warna aksen emas/amber, dipakai hemat -- gak ada gradient pelangi
-val OrangeAccent = Color(0xFFD8A650)
-val RedAccent = Color(0xFFD8A650)
-val PurpleAccent = Color(0xFFD8A650)
+// Satu warna aksen ungu, dipakai hemat -- gak ada gradient pelangi
+val OrangeAccent = Color(0xFF9B5DE5)
+val RedAccent = Color(0xFF9B5DE5)
+val PurpleAccent = Color(0xFF9B5DE5)
 
-val NeonBlue = Color(0xFFD8A650)
-val NeonPurple = Color(0xFFD8A650)
+val NeonBlue = Color(0xFF9B5DE5)
+val NeonPurple = Color(0xFF9B5DE5)
 
 val TextPrimary = Color(0xFFFFFFFF)
 val TextSecondary = Color(0xFFA6A6AC)
 val TextMuted = Color(0xFF6C6C72)
 
-val GoldAccent = Color(0xFFD8A650)
-val GoldAccentDim = Color(0xFF8A6E3B)
+val GoldAccent = Color(0xFF9B5DE5)
+val GoldAccentDim = Color(0xFF5E3A8C)
